0d7925d2fe2c44a5d912d1afccce3b3746361020
wglastonio <wglastonio@gmail.com>
2024-02-18 12:16:12 -0300

    feat: Improve content

--- src/app/repository/dataFake.ts
+++ src/app/repository/dataFake.ts
@@ -8,19 +8,22 @@ export const dataFake = [
     {
         "id": "2",
         "picture": "https://img.pokemondb.net/artwork/vector/large/bulbasaur.png",
-        "title": "Lorem ipsum sequi corrupti similique, veniam possimus vitae alias tempore voluptates!",
-        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolores ut fugiat officiis? Sequi maxime libero culpa alias harum delectus quos! Sequi tempora perspiciatis consectetur reprehenderit quaerat, libero nostrum omnis?"
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit!",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aperiam eveniet esse dolorem, eum illo quia sapiente itaque. Vero assumenda, est sunt reprehenderit sequi atque cum dolorem fuga natus aliquid."
     },
     {
         "id": "3",
         "picture": "https://img.pokemondb.net/artwork/vector/large/raichu.png",
-        "title": "Lorem ipsum sequi corrupti similique, veniam possimus vitae alias tempore voluptates!",
-        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolores ut fugiat officiis? Sequi maxime libero culpa alias harum delectus quos! Sequi tempora perspiciatis consectetur reprehenderit quaerat, libero nostrum omnis?"
+        "title": "Nemo, magni? Libero debitis ratione illo ex perspiciatis illum!",
+        "content": "Nemo, magni? Libero debitis ratione illo ex perspiciatis illum amet culpa veniam natus et commodi voluptates quos, sunt error eum praesentium quisquam consequuntur alias hic sint officiis enim temporibus unde."
     },
     {
         "id": "4",
         "picture": "https://img.pokemondb.net/artwork/vector/large/absol.png",
-        "title": "Lorem ipsum sequi corrupti similique, veniam possimus vitae alias tempore voluptates!",
-        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolores ut fugiat officiis? Sequi maxime libero culpa alias harum delectus quos! Sequi tempora perspiciatis consectetur reprehenderit quaerat, libero nostrum omnis?"
+        "title": "Incidunt, dolorum ratione consectetur sapiente nostrum!",
+        "content": "Incidunt, dolorum ratione consectetur sapiente nostrum nobis mollitia deleniti voluptatum velit perspiciatis, eveniet itaque magni porro quidem unde saepe accusamus impedit odio. Est reiciendis repudiandae laboriosam cum soluta at consectetur."
     }
 ]
+
+
+
